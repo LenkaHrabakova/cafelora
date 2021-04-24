326d9b5fb9631678259558f2921f7db89a517bfd
Lenka Ruzickova <lenka.hrabakova@gmail.com>
2021-04-24 13:45:44 +0200

    navigation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './style.css';
 
 console.log('funguju!');
+
+const navElement = document.querySelector('#nav-btn');
+
+const navigation = () => {
+  document.querySelector('nav').classList.toggle('nav-closed');
+};
+
+document.querySelectorAll('nav a').forEach((element) => {
+  element.addEventListener('click', navigation);
+});
+navElement.addEventListener('click', navigation);
+
+// //objednavani
+
+//   const orderButtonElement = document.querySelector('.order-btn');
+//   const addPrvek = document.querySelector('.drink__cup');
+//  orderButtonElement.addEventListener('click', () =>
+// {
+//     addPrvek.classList.add('drink__cup--selected');
+//     orderButtonElement.textContent = '' });
